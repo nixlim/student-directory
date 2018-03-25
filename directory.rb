bbc3d25fb5d017c92f40eaa4f1c01c5f71d84b61
nixlim <riabtchuk@gmail.com>
2018-03-25 21:45:11 +0100

    Step 8 : Exercise 3

--- directory.rb
+++ directory.rb
@@ -17,8 +17,8 @@ def print_header()
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].chr.downcase == "a"
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
   end
 end
 
