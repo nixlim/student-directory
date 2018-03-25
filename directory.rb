a37e6cbdbace535ab4f8ab378698c3ffc35488f3
nixlim <riabtchuk@gmail.com>
2018-03-25 22:09:28 +0100

    Step 8 : Exercise 9

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ def input_students()
   name = gets.chomp()
   while !name.empty? do
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}"
     name = gets.chomp()
   end
   students
@@ -23,7 +23,7 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great #{students.count == 1 ? "student" : "students"}"
 end
 
 students = input_students()
