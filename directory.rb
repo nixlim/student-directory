5cbe6b603447bbaf0b3974ca0a9db7366ce3536e
nixlim <riabtchuk@gmail.com>
2018-03-25 21:43:16 +0100

    Step 8 : Exercise 2

--- directory.rb
+++ directory.rb
@@ -18,7 +18,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].chr.downcase == "a"
   end
 end
 
