3fba2dd44bd79caeab27823dda427ac0aa906d3c
nixlim <riabtchuk@gmail.com>
2018-03-25 22:14:11 +0100

    Step 8 : Exercise 12

--- directory.rb
+++ directory.rb
@@ -17,8 +17,10 @@ def print_header()
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
+  if !students.empty?
+    students.each do |student|
+      puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
+    end
   end
 end
 
