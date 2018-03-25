916970a482fa49688ff0a1050a013fe517889df5
nixlim <riabtchuk@gmail.com>
2018-03-25 21:40:34 +0100

    Step 8 : Exercise 1

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-# students = [
-#     {name: "Dr. Hannibal Lecter", cohort: :november},
-#     {name: "Darth Vader", cohort: :november},
-#     {name: "Nurse Ratched", cohort: :november},
-#     {name: "Michael Corleone", cohort: :november},
-#     {name: "Alex DeLarge", cohort: :november},
-#     {name: "The Wicked Witch of the West", cohort: :november},
-#     {name: "Terminator", cohort: :november},
-#     {name: "Freddy Krueger", cohort: :november},
-#     {name: "The Joker", cohort: :november},
-#     {name: "Joffrey Baratheon", cohort: :november},
-#     {name: "Norman Bates", cohort: :november}
-# ]
-
 def input_students()
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -31,8 +17,8 @@ def print_header()
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
