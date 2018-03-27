b8f4b6a462e415648b5176d2b635b7ed129f299c
nixlim <riabtchuk@gmail.com>
2018-03-27 23:37:26 +0100

    adding file IO

--- directory.rb
+++ directory.rb
@@ -7,12 +7,25 @@ def interactive_menu
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line= student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+
 def process(selection)
   case selection
     when "1"
       input_students()
     when "2"
       show_students()
+    when "3"
+      save_students()
     when "9"
       exit
     else
@@ -29,6 +42,7 @@ end
 def print_menu
   puts "1. Input students"
   puts "2. Show students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
